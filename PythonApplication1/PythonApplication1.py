ab44f11c313edbe96c4b6a01c0e02b653ce5d6b8
Nikita <39918253+Giro5@users.noreply.github.com>
2018-09-21 00:14:06 +0500

    Add files via upload

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,18 @@
+import winreg
+key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\\ThisIsMyCompany")
+winreg.SetValue(key, "MyFolderLike", winreg.REG_SZ, "It's Subkey")
+winreg.SetValueEx(key, "JustValueName", 0 , winreg.REG_SZ, "My Value")
+key.Close()
+
+key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,"Software\\ThisIsMyCompany", 0, winreg.KEY_ALL_ACCESS)
+print("Значение подключа или что-то типа папки:", winreg.QueryValue(key,"MyFolderLike"))
+print("\nЗначение переменной или что-то типа того:", winreg.QueryValueEx(key,"JustValueName")[0])
+key.Close()
+
+key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam", 0, winreg.KEY_ALL_ACCESS)
+print("\nЗначение из программы Steam - последний использованный никнейм:", winreg.QueryValueEx(key, "LastGameNameUsed")[0])
+key.Close()
+
+input("\nДля продолжения нажмите любую клавишу")
+winreg.DeleteKey(winreg.HKEY_CURRENT_USER, "Software\\ThisIsMyCompany\\MyFolderLike")
+winreg.DeleteKey(winreg.HKEY_CURRENT_USER, "Software\\ThisIsMyCompany")
