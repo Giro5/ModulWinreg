6070dbd6bc6dc19e7e2c2504297667471053991a
Nikita <nikita-bat2000@mail.ru>
2018-09-21 00:18:32 +0500

    added try

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -9,10 +9,12 @@ print("Значение подключа или что-то типа папки:
 print("\nЗначение переменной или что-то типа того:", winreg.QueryValueEx(key,"JustValueName")[0])
 key.Close()
 
-key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam", 0, winreg.KEY_ALL_ACCESS)
-print("\nЗначение из программы Steam - последний использованный никнейм:", winreg.QueryValueEx(key, "LastGameNameUsed")[0])
-key.Close()
-
+try:
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam", 0, winreg.KEY_ALL_ACCESS)
+    print("\nЗначение из программы Steam - последний использованный никнейм:", winreg.QueryValueEx(key, "LastGameNameUsed")[0])
+    key.Close()
+except BaseException:
+    print("Неудалось вернуть значение из программы Steam")
 input("\nДля продолжения нажмите любую клавишу")
 winreg.DeleteKey(winreg.HKEY_CURRENT_USER, "Software\\ThisIsMyCompany\\MyFolderLike")
 winreg.DeleteKey(winreg.HKEY_CURRENT_USER, "Software\\ThisIsMyCompany")
